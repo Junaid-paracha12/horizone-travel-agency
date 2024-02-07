40838da46975f65db7e04c59a1337ca1d421e22e
Junaid-paracha-007 <junaidmaqbool009@gmail.com>
2024-02-07 12:01:04 +0500

    update-index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function index() {
   return (
     <div>
-      
+      <h1 className='text-4xl text-blue-500 font-bold'>Hello World Welcome to Horizone Travel Agency</h1>
     </div>
   )
 }
