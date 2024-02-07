19efd4cfb982ebe9f0fe56b27dddf55c1b8606aa
Junaid-paracha-007 <junaidmaqbool009@gmail.com>
2024-02-07 12:11:25 +0500

    hero-section-added

---
+++ components/Hero.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div>
+      <h1 className='text-5xl font-bold text-rose-600'>This is Hero Section</h1>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Hero from '@/components/Hero'
 import React from 'react'
 
 export default function index() {
   return (
     <div>
       <h1 className='text-4xl text-blue-500 font-bold'>Hello World Welcome to Horizone Travel Agency</h1>
+      <Hero />
     </div>
   )
 }
